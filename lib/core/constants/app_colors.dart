7e2c982218609b61b71d7483364097eb49d44ef9
AdeeshaNanayakkara <adeeshananayakkara27@gmail.com>
2026-06-12 14:04:40 +0530

    theme settings

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Brand Colors
+  static const Color primary = Color(0xFF170C79);
+  static const Color primaryDark = Color(0xFF0C064A);
+  static const Color primaryLight = Color(0xFF56B6C6);
+
+  static const Color secondary = Color(0xFF56B6C6);
+  static const Color secondaryDark = Color(0xFF328A97);
+  static const Color secondaryLight = Color(0xFF8ACBD0);
+
+  // Light Theme Colors
+  static const Color lightBackground = Color(0xFFFAF7F0);
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightCard = Color(0xFFFFFFFF);
+
+  static const Color lightTextPrimary = Color(0xFF170C79);
+  static const Color lightTextSecondary = Color(0xFF565E87);
+  static const Color lightTextDisabled = Color(0xFFA5ABC6);
+
+  static const Color lightBorder = Color(0xFFEFE3CA);
+  static const Color lightDivider = Color(0xFFF4EDE0);
+
+  // Dark Theme Colors
+  static const Color darkBackground = Color(0xFF0C064A);
+  static const Color darkSurface = Color(0xFF130D5A);
+  static const Color darkCard = Color(0xFF1A1469);
+
+  static const Color darkTextPrimary = Color(0xFFFAF7F0);
+  static const Color darkTextSecondary = Color(0xFF8ACBD0);
+  static const Color darkTextDisabled = Color(0xFF5E659E);
+
+  static const Color darkBorder = Color(0xFF26207A);
+  static const Color darkDivider = Color(0xFF1A1469);
+
+  // Status Colors
+  static const Color success = Color(0xFF10B981);
+  static const Color warning = Color(0xFFF59E0B);
+  static const Color error = Color(0xFFEF4444);
+  static const Color info = Color(0xFF3B82F6);
+
+  // E-commerce Specific Colors
+  static const Color discount = Color(0xFFDC2626);
+  static const Color rating = Color(0xFFFACC15);
+  static const Color inStock = Color(0xFF16A34A);
+  static const Color outOfStock = Color(0xFFEF4444);
+
+  // Common
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color transparent = Colors.transparent;
+}
